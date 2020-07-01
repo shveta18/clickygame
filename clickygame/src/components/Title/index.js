0472ff7b919bd2f35c375bc75b370750b9cb3fee
Shveta Kumar <sk@shveta.ca>
2020-07-01 18:29:56 -0400

    deployment to githun

--- clickygame/src/components/Title/index.js
+++ clickygame/src/components/Title/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title">{props.children}</h1>;
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <h1 className="title">{props.children}</h1>;
+</nav>
+        </div>
+
+    )
 }
 
 export default Title;
