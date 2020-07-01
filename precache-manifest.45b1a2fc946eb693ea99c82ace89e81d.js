a2bbabf6ebe882cdf4ab10fc10b724e713c0d778
Shveta Kumar <sk@shveta.ca>
2020-07-01 18:35:48 -0400

    Updates

---
+++ precache-manifest.45b1a2fc946eb693ea99c82ace89e81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585a92b1a7794184e9a633a481e16bf9",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "3e0bb2fe79e3fa3c6a7b",
+    "url": "/clickygame/static/css/main.622b8853.chunk.css"
+  },
+  {
+    "revision": "4d31a8f9307d446166c5",
+    "url": "/clickygame/static/js/2.186e8967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.186e8967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0bb2fe79e3fa3c6a7b",
+    "url": "/clickygame/static/js/main.7a3ec5a8.chunk.js"
+  },
+  {
+    "revision": "7122649522b9616ddb97",
+    "url": "/clickygame/static/js/runtime-main.71d67497.js"
+  }
+]);
